db0cb7e5f7d57a7b0c70678e5bb97983a95c4a14
Tommy Fannon <tfannon@gmail.com>
2016-08-08 07:04:47 -0400

    add sample code for adding track to library, querying playlists

--- AppleMusicTests/ViewController.swift
+++ AppleMusicTests/ViewController.swift
@@ -12,13 +12,54 @@ import MediaPlayer
 
 
 class ViewController: UIViewController {
-
-    @IBAction func addTrack(_ sender: AnyObject) {
-    }
     
     @IBOutlet weak var textView: UITextView!
-
     @IBOutlet weak var nowPlaying: UILabel!
+
+
+    @IBAction func addTrack(_ sender: AnyObject) {
+        guard let track = applePlayer.nowPlayingItem else {
+            show2(message: "No item in media player")
+            return
+        }
+        guard let artist = getValidArtist(track: track) else {
+            show2(message: "No valid artist")
+            return
+        }
+        guard let title = track.title else {
+            show2(message: "No valid track")
+            return
+        }
+            
+        ITunesApi.find(media: Media.Music)
+            .by(artist: artist, song: title)
+            .limit(limit: 1)
+            .request { json, error in
+                print (json)
+                //guard?
+                if let id = json?["results"][0]["trackId"].stringValue {
+                    self.mediaLibrary.addItem(withProductID: id) { result in
+                        guard let item = result.0[0] as? MPMediaItem else {
+                            self.show2(message: "Item not added")
+                            return
+                        }
+                        let message = self.getTrackInfo(track: item)
+                        self.show2(message: "Added: " + message)
+                    }
+                }
+        }
+    }
+    
+    func show2(message: String) {
+        let alert = UIAlertController(title: message, message: nil, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { result in }
+        alert.addAction(okAction)
+        DispatchQueue.main.sync {
+            self.present(alert, animated: true)
+        }
+    }
+
+
     
     
     @IBAction func queryPlaylists(_ sender: AnyObject) {
@@ -30,14 +71,12 @@ class ViewController: UIViewController {
             //let author = playlist.value(forProperty: MPMediaPlaylistPropertyAuthorDisplayName)
             let strPlaylist = textView.text!.appending("\(name!): \(playlist.count) songs\n")
             self.textView.text = strPlaylist
-            // for y in playlist.items {
-            //    let str2 = y.artist != nil ? y.artist : ""
-            // print ("\(y.albumArtist != nil ? y.albumArtist! : "") " +
-            // printArtist(albumArtist: y.albumArtist, artist: y.artist) +
-            // "\(y.albumTitle != nil ? y.albumTitle! : "") - " +
-            // "\(y.title != nil ? y.title! : "")" +
-            // "  Plays: \(y.playCount)")
-            // }
+            
+            for y in playlist.items {
+                let songInfo = "  " + getTrackInfo(track: y, showAlbum: false, showAlbumArtist: false) + " " + String(y.playCount) + "\n"
+                let strPlaylist = textView.text!.appending(songInfo)
+                self.textView.text = strPlaylist
+            }
             //
             //                    artist: \(y.artist)  album: \(y.albumTitle) track: \(y.title)  cloud: \(y.isCloudItem)  plays: \(y.playCount)  rating: \(y.rating)  date:\(y.releaseDate)") != nil)
         }
@@ -56,7 +95,9 @@ class ViewController: UIViewController {
     
     func songChanged() {
         if let track = applePlayer.nowPlayingItem {
-            nowPlaying.text = getTrackInfo(track: track)
+            let msg = getTrackInfo(track: track)
+            print (msg)
+            nowPlaying.text = msg
         }
     }
     
@@ -85,24 +126,38 @@ class ViewController: UIViewController {
         }
     }
     
-    func getTrackInfo(track: MPMediaItem) -> String {
+    func getTrackInfo(track: MPMediaItem, showAlbum: Bool = false, showAlbumArtist: Bool = false) -> String {
         var trackInfo: String = ""
-        if track.albumArtist != nil {
-            trackInfo += track.albumArtist!
+        if (showAlbumArtist) {
+            if track.albumArtist != nil {
+                trackInfo += track.albumArtist!
+            }
+            trackInfo += " : "
         }
-        trackInfo += " : "
         if track.artist != nil {
             trackInfo += "\(track.artist!)"
         }
         trackInfo += " : "
-        if track.albumTitle != nil {
-            trackInfo += "\(track.albumTitle!)"
+        if (showAlbum) {
+            if track.albumTitle != nil {
+                trackInfo += "\(track.albumTitle!)"
+            }
+            trackInfo += " : "
         }
-        trackInfo += " : "
         if track.title != nil {
             trackInfo += "\(track.title!)"
         }
         return trackInfo
+    }
+    
+    func getValidArtist(track: MPMediaItem) -> String? {
+        if track.artist != nil {
+            return track.artist!
+        }
+        if track.albumArtist != nil {
+            return track.albumArtist!
+        }
+        return nil
     }
 
 
